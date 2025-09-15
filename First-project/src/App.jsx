9f9642e77d954745f0aaa0cc3acd5ba956597ea6
tech-code20 <mdmuntaziralamkne@gmail.com>
2025-09-15 20:21:06 +0530

    First project done

--- First-project/src/App.jsx
+++ First-project/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [Counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    if(Counter === 20) setCounter(Counter), alert(`Maximum counted e.i., ${Counter}`)
+    else setCounter(Counter + 1)
+  }
+  const removeValue = () => {
+    if(Counter === 0) setCounter(Counter), alert("Counter can't be negative")
+    else setCounter(Counter - 1)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Muntazir</h1>
+      <h2>Counter value: {Counter}</h2>
+      <button
+      onClick={addValue}
+      >Increase Value</button>
+      <button
+      onClick={removeValue}
+      >Decrease Value</button>
     </>
   )
 }
